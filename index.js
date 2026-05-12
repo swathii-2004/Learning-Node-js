2a8d2410d592fbdb6862dd1e05f5d9713c0f2e95
swathi <swathipoojary1864@gmail.com>
2026-05-12 20:14:23 +0530

    created endpoints

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.get("/name", (req, res) => {
+    res.send("my name is sonu")
+})
+
+app.get("/age", (req, res) => {
+    res.send("my age is 21")
+})
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
+
